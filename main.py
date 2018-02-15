c97c5e80b381cc5ac6daf7b0a77cf1dd275d120e
bernard169 <bernard@tourneur.org>
2018-02-15 10:10:55 +0100

    define findDigits (path)

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ pattern2 = "^\+?\-?\d+$"
 compare (pattern2, "integer" )
 
 #c
-pattern3 = "^[1-9]?[A-Z]{3}\d{3}$"
-pattern3b = "^[1-9]?\d{3}[A-Z]{3}$"
+pattern3 = "^[1-9]?[A-Z]{3}\d{3}|[1-9]?\d{3}[A-Z]{3}$"
 compare (pattern3, "plate number")
 
 #d
@@ -29,3 +28,29 @@ pattern4 = r"^[A-Z]{1}:\\"
 compare (pattern4, "nameSpace")
 
 #Exercice 2 
+def findDigits (path):
+    result =""
+    digits = "\d +"
+    d = re.compile (digits)
+    try :
+        with open (path) as file :
+            tabledFile = file.readlines ()
+            i=0
+            while i < len (tabledFile):
+                line = tabledFile [i]
+                if d.match (line) is not None:
+                    result += "line {} : ".format (str (i))
+                    digitsInLine = d.findall (line)
+                    for number in digitsInLine : 
+                        result += number + ", "
+                i+=1
+            return result
+    except IOError:
+        print ("incorrect path")
+        newPath = input ("enter a new path : ")
+        findDigits (newPath)
+
+myPath = input ("enter a path : ")
+print (findDigits (myPath))
+
+
