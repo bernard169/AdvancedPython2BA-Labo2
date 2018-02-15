8d440c30382535b60deada443449cd345dee291e
bernard169 <bernard@tourneur.org>
2018-02-15 09:31:33 +0100

    edit compare function, line 10

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def compare (myPattern, patternNature):
     if p.match (ask) is not None : 
         print ("Your {} is :".format (patternNature), ask)
     else :
-        while p1.match (ask) is None :
+        while p.match (ask) is None :
              print ("Please enter your {} correctly".format (patternNature))
              ask = input ("Please enter your {}: ".format (patternNature))
         print ("Your {} is :".format (patternNature), ask) 
@@ -20,7 +20,12 @@ pattern2 = "^\+?\-?\d+$"
 compare (pattern2, "integer" )
 
 #c
-
 pattern3 = "^[1-9]?[A-Z]{3}\d{3}$"
 pattern3b = "^[1-9]?\d{3}[A-Z]{3}$"
 compare (pattern3, "plate number")
+
+#d
+pattern4 = r"^[A-Z]{1}:\\"
+compare (pattern4, "nameSpace")
+
+#Exercice 2 
