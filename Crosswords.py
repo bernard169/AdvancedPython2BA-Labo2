3c8eb621269559588b3a185bf617c4258d3219f6
bernard169 <bernard@tourneur.org>
2018-02-16 18:53:31 +0100

    simplify checkRegexCrossword

--- Crosswords.py
+++ Crosswords.py
@@ -3,26 +3,18 @@
 import re
 
 def checkRegexCrossword (linesRegex, columnsRegex, answer):
-    isCorrect = False
-    linesOK = True
-    columnsOK = True
+    isCorrect = True
     for line in linesRegex: #If each line matches the answer, linesOK is ultimately True
         l = re.compile (line)
-        if l.match (answer [linesRegex.index (line)]) is not None:
-            linesOk = True
-        else :
+        if l.match (answer [linesRegex.index (line)]) is None:
             return False
     for column in columnsRegex : #Same as for the lines
         c = re.compile (column)
         ansCol = "" #Contains the answer's columns, to be compared to columnsRegex
         for line in answer :
             ansCol += line [columnsRegex.index (column)]
-        if c.match (ansCol) is not None :
-            columnsOK = True
-        else :
+        if c.match (ansCol) is None :
             return False
-    if linesOK and columnsOK :
-        isCorrect = True
     return isCorrect
 
 print (checkRegexCrossword (["EC|CD[ABS]",".[GROS]+","C?[KS]+"],
